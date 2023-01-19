65d9c9ce1ef0aef74c8b76ee85e203b7438029fa
Therakar <matteo.pau200@gmail.com>
2023-01-19 16:32:30 +0100

    crea e lancia la migration

---
+++ database/migrations/2023_01_19_153118_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description', 1000);
+            $table->string('thumb', 200);
+            $table->float('price', 4,2);
+            $table->string('series', 30);
+            $table->dateTime('sale_date');
+            $table->string('type', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
